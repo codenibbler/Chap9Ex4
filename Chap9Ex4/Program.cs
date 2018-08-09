0d487825b76cafa415a4a17378f586635488013d
Brian Banahan <brianbanahan@gmail.com>
2018-08-09 14:46:52 +0100

    Fin

--- Chap9Ex4/Program.cs
+++ Chap9Ex4/Program.cs
@@ -10,7 +10,32 @@ namespace Chap9Ex4
     {
         static void Main(string[] args)
         {
-            // First edit.
+            // Write a method that finds how many times certain number can be
+            // found in a given array. Write a program to test that the method works
+            // correctly.
+
+            int[] testArr = { 1, 4, 5, 5, 6, 7, 8, 9, 4, 9, 9, 9, 3 };
+
+            int.TryParse(Console.ReadLine(), out int number);
+            Console.WriteLine(numOccur(testArr, number));
+            Console.ReadLine();
+
+
+        }
+
+        static int numOccur(int[] inputArr, int number)
+        {
+            int occur = 0;
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                if (inputArr[i] == number)
+                {
+                    occur++;
+                }
+            }
+
+            return occur;
         }
     }
 }
